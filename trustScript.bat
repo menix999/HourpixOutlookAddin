a0834a3132bd3b9b7f83b43bdd6166bd94a7f9a1
Damian Smoliński <damiansmolinski32@gmail.com>
2024-08-08 10:30:11 +0200

    add trustScript

---
+++ trustScript.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+rem Definiowanie ścieżki rejestru dla zaufanych witryn
+set "registryKey=HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\github.io\menix999"
+
+rem Sprawdzenie, czy ścieżka rejestru istnieje
+reg query "%registryKey%" >nul 2>&1
+if %errorlevel% neq 0 (
+    rem Utworzenie klucza rejestru, jeśli nie istnieje
+    reg add "%registryKey%" /f
+)
+
+rem Dodanie subdomeny do zaufanych (wartość 2 oznacza zaufaną witrynę)
+reg add "%registryKey%" /v "*" /t REG_DWORD /d 2 /f
+
+echo Subdomena https://*.menix999.github.io została dodana do zaufanych witryn.
+pause
+
+endlocal
+exit /b
