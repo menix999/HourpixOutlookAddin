0c4d1deb4ba8068e9c3e1dec340e9ad9a086d4c6
Damian Smoliński <damiansmolinski32@gmail.com>
2024-08-08 13:03:04 +0200

    asd

--- trustScript.bat
+++ trustScript.bat
@@ -11,9 +11,11 @@ if %errorlevel% neq 0 (
     reg add "%registryKey%" /f
 )
 
-rem Dodanie subdomeny do zaufanych (wartość 2 oznacza zaufaną witrynę)
+rem Dodanie witryny do zaufanych (wartość 2 oznacza zaufaną witrynę)
 reg add "%registryKey%" /v "*" /t REG_DWORD /d 2 /f
 
-rem Skrypt zakończył działanie, automatycznie się zamyka
+echo Strona https://menix999.github.io została dodana do zaufanych witryn.
+pause
+
 endlocal
 exit /b
